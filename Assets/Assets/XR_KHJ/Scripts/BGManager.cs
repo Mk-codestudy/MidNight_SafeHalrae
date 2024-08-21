52e3b79e6cd2dcccdc59ea7ebc0f3a1959466101
hzkkong <hz0227798@gmail.com>
2024-08-21 23:11:46 +0900

    오디오 작업중

--- Assets/Assets/XR_KHJ/Scripts/BGManager.cs
+++ Assets/Assets/XR_KHJ/Scripts/BGManager.cs
@@ -9,12 +9,12 @@ public class BGManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.Play();
+        audioSource = GetComponent<AudioSource>();       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        audioSource.Play();
     }
 }
